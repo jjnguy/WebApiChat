76398df531b9e2f229a73cc9e7e26ce06a75fbd3
Justin Nelson <justin.john.nelson@gmail.com>
2014-03-30 20:51:33 -0500

    bootstrap source!!!

---
+++ WebApiChat/Controllers/LogonController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApiChat.Controllers
+{
+    public class LogonController : Controller
+    {
+        //
+        // GET: /Logon/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
